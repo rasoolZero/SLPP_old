65f1ecdbdf8dd63d61749c01948268104dbf5e85
rasoolZero <rasool.kamkar20@gmail.com>
2021-07-16 04:48:35 +0430

    clean up

--- CButton.cpp
+++ CButton.cpp
@@ -1,1 +1,30 @@
 #include "CButton.h"
+void CButton::openFile(){
+    tgui::FileDialog::Ptr fileDialog = tgui::FileDialog::create("Open Audio File (.wav,.flac,.ogg)");
+    fileDialog->setFileTypeFilters({{"Audio Files (wav,ogg,flac)", {"*.wav", "*.ogg","*.flac"}}});
+    fileDialog->setFileMustExist(true);
+    fileDialog->onFileSelect(&CButton::fileSelect,this);
+    tgui::Container * panel;
+    panel = this->getParent();
+    while(panel->getWidgetName()!="panel"){
+        panel = panel->getParent();
+    }
+
+    panel->getParent()->add(fileDialog,"fileDialog");
+    panel->setEnabled(false);
+}
+void CButton::fileSelect(const std::vector<tgui::Filesystem::Path>& paths){
+    if(paths.size()>=1){
+        sf::String sfStr(paths[0].asString());
+        if(buffer.loadFromFile(sfStr)){
+            AudioLoaded = true;
+
+        }
+    }
+    tgui::Container * panel;
+    panel = this->getParent();
+    while(panel->getWidgetName()!="panel"){
+        panel = panel->getParent();
+    }
+    panel->setEnabled(true);
+}
--- CButton.h
+++ CButton.h
@@ -30,36 +30,9 @@ class CButton : public tgui::Button
         else
             return nullptr;
     }
-    void openFile(){
-        tgui::FileDialog::Ptr fileDialog = tgui::FileDialog::create("Open Audio File (.wav,.flac,.ogg)");
-        fileDialog->setFileTypeFilters({{"Audio Files (wav,ogg,flac)", {"*.wav", "*.ogg","*.flac"}}});
-        fileDialog->setFileMustExist(true);
-        fileDialog->onFileSelect(&CButton::fileSelect,this);
-        tgui::Container * panel;
-        panel = this->getParent();
-        while(panel->getWidgetName()!="panel"){
-            panel = panel->getParent();
-        }
+    void openFile();
 
-        panel->getParent()->add(fileDialog,"fileDialog");
-        panel->setEnabled(false);
-    }
-
-    void fileSelect(const std::vector<tgui::Filesystem::Path>& paths){
-        if(paths.size()>=1){
-            sf::String sfStr(paths[0].asString());
-            if(buffer.loadFromFile(sfStr)){
-                AudioLoaded = true;
-
-            }
-        }
-        tgui::Container * panel;
-        panel = this->getParent();
-        while(panel->getWidgetName()!="panel"){
-            panel = panel->getParent();
-        }
-        panel->setEnabled(true);
-    }
+    void fileSelect(const std::vector<tgui::Filesystem::Path>& paths);
 
 protected:
 
